d58dfc21b82bd2c319dd71982106e3af8e6cd8fb
Darius Wattimena <dariuswattimena@hotmail.com>
2024-01-03 22:23:25 +0100

    Added a script to build and start up MV inside the IDE

---
+++ build.bat
@@ -0,0 +1,2 @@
+dotnet build ./MapsetChecksCatch.sln
+move C:\Users\dariu\source\repos\MapsetChecksCatch\bin\Debug\netcoreapp3.1\MapsetChecksCatch.dll "C:\Users\dariu\AppData\Roaming\Mapset Verifier Externals\checks"
